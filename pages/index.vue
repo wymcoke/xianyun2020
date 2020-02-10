5d74182ecfe9e85b7dbcae0cd59ee4a02787ae7f
可乐很苦 <635347966@qq.com>
2020-02-10 21:59:51 +0800

    5.完成首页布局

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,22 @@
         ></div>
       </el-carousel-item>
     </el-carousel>
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span v-for="(item, index) in options" :key="index" @click="handleClick(index)" :class="{active:current === index}">
+            <i>{{ item.text }}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input :placeholder="options[current].placeholder" />
+          <i class="el-icon-search"></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,19 +37,48 @@
 export default {
   data() {
     return {
-      // 轮播图数据
-      banners: []
+      banners: [], // 轮播图数据
+      //tab栏数据
+      options: [
+        {
+          text: "攻略",
+          placeholder: "搜索城市"
+        },
+        {
+          text: "酒店",
+          placeholder: "请输入城市以搜索酒店"
+        },
+        {
+          text: "机票",
+          placeholder: "搜索机票"
+        }
+      ],
+      //重要的索引
+      current: 0
     };
   },
+
   mounted() {
-    // 请求轮播图数据
+    //请求轮播图数据
     this.$axios({
       url: "/scenics/banners"
     }).then(res => {
-      // data轮播图的数据
+      //
       const { data } = res.data;
       this.banners = data;
     });
+  },
+  methods: {
+    //点击tab栏按钮时候的事件,重点修改current
+    handleClick(index) {
+      //点击的如果是机票的按钮
+      if(index === 2){
+        this.$router.push({
+          path:"/air"
+        })
+      }
+      this.current = index;
+    }
   }
 };
 </script>
@@ -51,6 +96,95 @@ export default {
   .banner-image {
     width: 100%;
     height: 100%;
+  }
+
+  .banner-content {
+    z-index: 9;
+    width: 1000px;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    margin-left: -500px;
+    border-top: 1px transparent solid;
+
+    .search-bar {
+      width: 552px;
+      margin: 0 auto;
+    }
+
+    .search-tab {
+      .active {
+        i {
+          color: #333;
+        }
+        &::after {
+          background: #eee;
+        }
+      }
+
+      span {
+        width: 82px;
+        height: 36px;
+        display: block;
+        position: relative;
+        margin-right: 8px;
+        cursor: pointer;
+
+        i {
+          position: absolute;
+          z-index: 2;
+          display: block;
+          width: 100%;
+          height: 100%;
+          line-height: 30px;
+          text-align: center;
+          color: #fff;
+        }
+
+        &:after {
+          position: absolute;
+          left: 0;
+          top: 0;
+          display: block;
+          content: "";
+          width: 100%;
+          height: 100%;
+          border: 1px rgba(255, 255, 255, 0.2) solid;
+          border-bottom: none;
+          transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+          transform-origin: bottom left;
+          background: rgba(0, 0, 0, 0.5);
+          border-radius: 1px 2px 0 0;
+          box-sizing: border-box;
+        }
+      }
+    }
+
+    .search-input {
+      width: 550px;
+      height: 46px;
+      background: #fff;
+      border-radius: 0 4px 4px 4px;
+      border: 1px rgba(255, 255, 255, 0.2) solid;
+      border-top: none;
+      box-sizing: unset;
+
+      input {
+        flex: 1;
+        height: 20px;
+        padding: 13px 15px;
+        outline: none;
+        border: 0;
+        font-size: 16px;
+      }
+
+      .el-icon-search {
+        cursor: pointer;
+        font-size: 22px;
+        padding: 0 10px;
+        font-weight: bold;
+      }
+    }
   }
 }
 </style>
