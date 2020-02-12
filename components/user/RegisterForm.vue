4f8d22304683ee7b0fe61c6d47a79beed6868205
可乐很苦 <635347966@qq.com>
2020-02-12 20:54:00 +0800

    提交注册和添加axios插件

--- components/user/RegisterForm.vue
+++ components/user/RegisterForm.vue
@@ -4,21 +4,19 @@
   <!-- rules: 表单验证规则 -->
   <el-form :model="form" ref="form" :rules="rules" class="form">
     <el-form-item class="form-item" prop="username">
-      <el-input v-model="form.username" placeholder="用户名手机"> </el-input>
+      <el-input v-model="form.username" placeholder="用户名手机"></el-input>
     </el-form-item>
 
     <el-form-item class="form-item" prop="captcha">
       <el-input v-model="form.captcha" placeholder="验证码">
         <template slot="append">
-          <el-button @click="handleSendCaptcha">
-            发送验证码
-          </el-button>
+          <el-button @click="handleSendCaptcha">发送验证码</el-button>
         </template>
       </el-input>
     </el-form-item>
 
     <el-form-item class="form-item" prop="nickname">
-      <el-input v-model="form.nickname" placeholder="昵称"> </el-input>
+      <el-input v-model="form.nickname" placeholder="昵称"></el-input>
     </el-form-item>
 
     <el-form-item class="form-item" prop="password">
@@ -34,13 +32,12 @@
         v-model="form.checkPassword"
         placeholder="确认密码"
         type="password"
-      >
-      </el-input>
+      ></el-input>
     </el-form-item>
 
-    <el-button class="submit" type="primary" @click="handleRegSubmit">
-      注册
-    </el-button>
+    <el-button class="submit" type="primary" @click="handleRegSubmit"
+      >注册</el-button
+    >
   </el-form>
 </template>
 
@@ -71,6 +68,17 @@ export default {
         callback();
       }
     };
+    // 验证手机号码
+    const validateUsername = (rule, value, callback) => {
+      if (value == "") {
+        callback(new Error("请再次手机号码"));
+      } else if (/^1[3-9][0-9]{9}$/.test(value) == false) {
+        // 判断是否是手机号码的正则
+        callback(new Error("手机号码格式错误"));
+      } else {
+        callback();
+      }
+    };
     return {
       // 表单数据
       form: {
@@ -84,7 +92,8 @@ export default {
       rules: {
         username: [
           // required表示必填，message错误的提示信息. trigger输入框失去焦点时候触发验证
-          { required: true, message: "请输入用户名", trigger: "blur" }
+          // { required: true, message: "请输入用户名", trigger: "blur" }
+          { validator: validateUsername, trigger: "blur" }
         ],
         captcha: [{ required: true, message: "请输入验证码", trigger: "blur" }],
         nickname: [{ required: true, message: "请输入昵称", trigger: "blur" }],
@@ -113,8 +122,14 @@ export default {
       this.$refs.form.validate(valid => {
         // 表单验证全部通过
         if (valid) {
-          // 调用注册的接口
-          console.log(this.form);
+          // props是除了checkPassword以外剩余的属性
+          const { checkPassword, ...data } = this.form;
+          // 调用store仓库中user模块下注册的方法
+          this.$store.dispatch("user/register", data).then(res => {
+            this.$message.success("注册成功");
+            // 跳转到首页
+            this.$router.push("/");
+          });
         }
       });
     }
