271351cd7f378b1f281aa91758d24f29332bc389
可乐很苦 <635347966@qq.com>
2020-02-12 19:44:25 +0800

    渲染用户数据

--- components/header.vue
+++ components/header.vue
@@ -7,7 +7,6 @@
           <img src="http://157.122.54.189:9093/images/logo.jpg" alt="" />
         </nuxt-link>
       </div>
-
       <!-- 菜单栏 -->
       <el-row type="flex" class="navs">
         <nuxt-link to="/">首页</nuxt-link>
@@ -15,15 +14,19 @@
         <nuxt-link to="/hotel">酒店</nuxt-link>
         <nuxt-link to="/air">国内机票</nuxt-link>
       </el-row>
-
       <!-- 登录/用户信息 -->
       <el-row type="flex" align="middle">
         <!-- 如果用户存在则展示用户信息，用户数据来自store -->
-        <el-dropdown v-if="false">
+        <el-dropdown v-if="$store.state.user.userInfo.token">
           <el-row type="flex" align="middle" class="el-dropdown-link">
             <nuxt-link to="#">
-              <img src="http://157.122.54.189:9093/images/pic_sea.jpeg" />
-              用户名
+              <img
+                :src="
+                  $axios.defaults.baseURL +
+                    $store.state.user.userInfo.user.defaultAvatar
+                "
+              />
+              {{ $store.state.user.userInfo.user.nickname }}
             </nuxt-link>
             <i class="el-icon-caret-bottom el-icon--right"></i>
           </el-row>
@@ -36,12 +39,10 @@
             </el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
-
         <!-- 不存在用户信息展示登录注册链接 -->
         <nuxt-link to="/user/login" class="account-link" v-else>
           登录 / 注册
         </nuxt-link>
-        {{ $store.state.user.userInfo.user.nickname }}
       </el-row>
     </el-row>
   </header>
