c3f3b67dde438e9f23e23e46d141082517330e41
可乐很苦 <635347966@qq.com>
2020-02-12 22:54:48 +0800

    列表渲染

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'} 
+      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css' }
     ]
   },
   /*
@@ -52,8 +52,8 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: "http://157.122.54.189:9095" // 新增备用地址
-    // baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径 		
+    // baseURL: "http://157.122.54.189:9095" // 新增备用地址
+    baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径 		
   },
   /*
   ** Build configuration
@@ -63,7 +63,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
---
+++ pages/air/flights.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
+
+        <!-- 航班头部布局 -->
+        <FlightsListHead />
+
+        <!-- 航班信息 -->
+        <FlightsItem
+          v-for="(item, index) in flightsData.flights"
+          :key="index"
+          :data="item"
+        />
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
+</template>
+
+<script>
+// 导入组件
+import FlightsListHead from "@/components/air/flightsListHead";
+import FlightsItem from "@/components/air/flightsItem";
+export default {
+  data() {
+    return {
+      // 机票总数据 （有info, flights, total, options这些属性）
+      flightsData: {}
+    };
+  },
+  components: {
+    FlightsListHead,
+    FlightsItem
+  },
+  mounted() {
+    // 请求机票列表数据
+    this.$axios({
+      url: "/airs",
+      params: this.$route.query
+    }).then(res => {
+      // 总数据
+      this.flightsData = res.data;
+    });
+  }
+};
+</script>
+
+<style scoped lang="less">
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
+.aside {
+  width: 240px;
+}
+</style>
