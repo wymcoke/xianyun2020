698c6d4dabd2ac0c3f0b2b7483ccc61808b16ff6
可乐很苦 <635347966@qq.com>
2020-02-16 21:27:05 +0800

    撤销筛选

--- components/air/flightsFilters.vue
+++ components/air/flightsFilters.vue
@@ -160,44 +160,49 @@ export default {
     // 选择机场时候触发
     handleAirport(value) {
       // 从所有出发机场里面找到条件符合value的
-    //   const newData = this.data.flights.filter(v => {
-    //     // 如果return的值是true，说明是符合条件
-    //     return v.org_airport_name === value;
-    //   });
-    //   this.$emit("getData", newData);
+      //   const newData = this.data.flights.filter(v => {
+      //     // 如果return的值是true，说明是符合条件
+      //     return v.org_airport_name === value;
+      //   });
+      //   this.$emit("getData", newData);
     },
     // 选择出发时间时候触发
     handleFlightTimes(value) {
-    //   const arr = value.split(","); // ["6","12"]
-    //   const newData = this.data.flights.filter(v => {
-    //     // 出发时间的小时
-    //     const hours = Number(v.dep_time.split(":")[0]);
-    //     // 判断出发的小时是否在选中的时间段内
-    //     return Number(arr[0]) <= hours && hours < Number(arr[1]);
-    //   });
-    //   this.$emit("getData", newData);
+      //   const arr = value.split(","); // ["6","12"]
+      //   const newData = this.data.flights.filter(v => {
+      //     // 出发时间的小时
+      //     const hours = Number(v.dep_time.split(":")[0]);
+      //     // 判断出发的小时是否在选中的时间段内
+      //     return Number(arr[0]) <= hours && hours < Number(arr[1]);
+      //   });
+      //   this.$emit("getData", newData);
     },
     // 选择航空公司时候触发
     handleCompany(value) {
       // 从所有航班里面找到条件符合value的 （假设是东航）
-    //   const newData = this.data.flights.filter(v => {
-    //     // 如果return的值是true，说明是符合条件
-    //     return v.airline_name === value;
-    //   });
-    //   this.$emit("getData", newData);
+      //   const newData = this.data.flights.filter(v => {
+      //     // 如果return的值是true，说明是符合条件
+      //     return v.airline_name === value;
+      //   });
+      //   this.$emit("getData", newData);
     },
     // 选择机型时候触发
     handleAirSize(value) {
       // 从所有航班里面找到条件符合value的 （假设是东航）
-    //   const newData = this.data.flights.filter(v => {
-    //     // 如果return的值是true，说明是符合条件
-    //     return v.plane_size === value;
-    //   });
-    //   this.$emit("getData", newData);
+      //   const newData = this.data.flights.filter(v => {
+      //     // 如果return的值是true，说明是符合条件
+      //     return v.plane_size === value;
+      //   });
+      //   this.$emit("getData", newData);
     },
 
     // 撤销条件时候触发
-    handleFiltersCancel() {}
+    handleFiltersCancel() {
+      this.airport = ""; // 机场
+      this.flightTimes = ""; // 出发时间
+      this.company = ""; // 航空公司
+      this.airSize = ""; // 机型大小
+    }
   }
 };
 </script>
